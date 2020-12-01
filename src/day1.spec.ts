8f45dbcab3d1a8c6f8e909db711172e3bdff123e
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-12-01 13:25:33 +0100

    formatting, cleaning

--- src/day1.spec.ts
+++ src/day1.spec.ts
@@ -2,31 +2,39 @@ import fs from 'fs'
 import path from 'path'
 
 describe('Day 1 - expense report', () => {
+    function part1_naiveSolution(input: string[]): number {
+        for (const inumber1 of input) {
+            for (const inumber2 of input) {
+                const n1: number = +inumber1
+                const n2: number = +inumber2
+                if ((n1 + n2) === 2020) return (n1 * n2)
+            }
+        }
+    }
+
+    function part2_naiveSolution(input: string[]): number {
+        for (const inumber1 of input) {
+            for (const inumber2 of input) {
+                for (const inumber3 of input) {
+                    const n1: number = +inumber1
+                    const n2: number = +inumber2
+                    const n3: number = +inumber3
+                    if ((n1 + n2 + n3) === 2020) return (n1 * n2 * n3)
+                }
+            }
+        }
+    }
+
     it('part 1 - sum to given number', () => {
         const inputStr = fs.readFileSync(path.join(__dirname, 'day1.input')).toString()
         const numbers = inputStr.split('\r\n')
 
-        for (const inumber1 of numbers) {
-            for (const inumber2 of numbers) {
-                const n1: number = +inumber1
-                const n2: number = +inumber2
-                if((n1 + n2) === 2020) console.log(n1 * n2)
-            }
-        }
-    });
+        console.log(part1_naiveSolution(numbers))
+    })
     it('part 2 - sum of 3 to given number', () => {
         const inputStr = fs.readFileSync(path.join(__dirname, 'day1.input')).toString()
         const numbers = inputStr.split('\r\n')
 
-        for (const inumber1 of numbers) {
-            for (const inumber2 of numbers) {
-                for (const inumber3 of numbers) {
-                    const n1: number = +inumber1
-                    const n2: number = +inumber2
-                    const n3: number = +inumber3
-                    if((n1 + n2 + n3) === 2020) console.log(n1 * n2 * n3)
-                }
-            }
-        }
-    });
+        console.log(part2_naiveSolution(numbers))
+    })
 })
