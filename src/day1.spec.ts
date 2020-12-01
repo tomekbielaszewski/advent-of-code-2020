cf5779041cd6f43891afbaffe56c55f5825a3d25
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-12-01 13:55:31 +0100

    "faster" solution xD

--- src/day1.spec.ts
+++ src/day1.spec.ts
@@ -25,16 +25,42 @@ describe('Day 1 - expense report', () => {
         }
     }
 
+    function part1_fasterSolution(input: string[]): number {
+        const hashset = new Set()
+        for (const elem of input) {
+            const num: number = +elem
+            if (hashset.has(2020 - num)) return num * (2020 - num)
+            hashset.add(num)
+        }
+    }
+
+    function part2_fasterSolution(input: string[]): number {
+        const hashset = new Set()
+        for (const elem1 of input) {
+            for (const elem2 of input) {
+                const num1: number = +elem1
+                const num2: number = +elem2
+                if (hashset.has(2020 - num1 - num2)) return num1 * num2 * (2020 - num1 - num2)
+                hashset.add(num1)
+                hashset.add(num2)
+            }
+        }
+    }
+
     it('part 1 - sum to given number', () => {
         const inputStr = fs.readFileSync(path.join(__dirname, 'day1.input')).toString()
         const numbers = inputStr.split('\r\n')
 
-        console.log(part1_naiveSolution(numbers))
+        const start = new Date().getTime()
+        console.log(`naive solution: ${part1_naiveSolution(numbers)} calculated in ${new Date().getTime() - start}ms`)
+        console.log(`faster solution: ${part1_fasterSolution(numbers)} calculated in ${new Date().getTime() - start}ms`)
     })
     it('part 2 - sum of 3 to given number', () => {
         const inputStr = fs.readFileSync(path.join(__dirname, 'day1.input')).toString()
         const numbers = inputStr.split('\r\n')
 
-        console.log(part2_naiveSolution(numbers))
+        const start = new Date().getTime()
+        console.log(`naive solution: ${part2_naiveSolution(numbers)} calculated in ${new Date().getTime() - start}ms`)
+        console.log(`faster solution: ${part2_fasterSolution(numbers)} calculated in ${new Date().getTime() - start}ms`)
     })
 })
