c2567e8473ef56fcf78c40c68c08070923e0afd7
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-12-02 10:07:10 +0100

    day 2 part 1 - checking occurrence of a letter in password

--- src/day2.spec.ts
+++ src/day2.spec.ts
@@ -13,16 +13,29 @@ describe('Day 2 - password policy', () => {
             let letterCounter = 0
 
             for (const passLetter of password) {
-                if(passLetter === letter) letterCounter++
+                if (passLetter === letter) letterCounter++
             }
 
-            if(letterCounter >= rangeFrom && letterCounter <= rangeTo) passCounter++
+            if (letterCounter >= rangeFrom && letterCounter <= rangeTo) passCounter++
         }
         return passCounter
     }
 
     function part2(input: string[]): number {
-        return 0
+        let passCounter = 0
+        for (const line of input) {
+            const segments = line.split(" ")
+            const firstPos = +segments[0].split("-")[0]
+            const secondPos = +segments[0].split("-")[1]
+            const letter = segments[1].charAt(0)
+            const password = segments[2]
+            let occurrenceCounter = 0
+
+            if (password.charAt(firstPos - 1) === letter) occurrenceCounter++
+            if (password.charAt(secondPos - 1) === letter) occurrenceCounter++
+            if(occurrenceCounter === 1) passCounter++
+        }
+        return passCounter
     }
 
     it('part 1 - sample', () => {
@@ -43,8 +56,18 @@ describe('Day 2 - password policy', () => {
         console.log(`solution: ${part1(numbers)} calculated in ${new Date().getTime() - start}ms`)
     })
 
-    it('part 2 - sum of 3 to given number', () => {
-        const inputStr = fs.readFileSync(path.join(__dirname, 'day1.input')).toString()
+    it('part 2 - sample', () => {
+        const inputStr = "1-3 a: abcde\r\n" +
+            "1-3 b: cdefg\r\n" +
+            "2-9 c: ccccccccc"
+        const numbers = inputStr.split('\r\n')
+
+        const start = new Date().getTime()
+        console.log(`solution: ${part2(numbers)} calculated in ${new Date().getTime() - start}ms`)
+    })
+
+    it('part 2 - check if a letter appears on ONE of given positions', () => {
+        const inputStr = fs.readFileSync(path.join(__dirname, 'day2.input')).toString()
         const numbers = inputStr.split('\r\n')
 
         const start = new Date().getTime()
