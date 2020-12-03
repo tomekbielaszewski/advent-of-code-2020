483414e06017086ae924afc69a4db8b6dceb7470
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-12-03 22:13:09 +0100

    day 3 part 2 - checking multiple slopes

--- src/day3.spec.ts
+++ src/day3.spec.ts
@@ -2,23 +2,30 @@ import fs from 'fs'
 import path from 'path'
 
 describe('Day 2 - Toboggan Trajectory', () => {
-    function part1(input: string[]): number {
+    function part1(input: string[], right: number, down:number): number {
         let counter = 0
         let verticalOffset = 0
         // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < input.length; i += down) {
             const line = input[i];
             if(line.charAt(verticalOffset) === '#') counter++
-            verticalOffset += 3
+            verticalOffset += right
             verticalOffset %= line.length
         }
         return counter
     }
 
     function part2(input: string[]): number {
-        let counter = 0
-        for (const line of input) {
-            counter = 1
+        let counter = 1
+        const slopes = [
+            [1,1],
+            [3,1],
+            [5,1],
+            [7,1],
+            [1,2],
+        ]
+        for (const slope of slopes) {
+            counter *= part1(input, slope[0], slope[1])
         }
         return counter
     }
@@ -38,7 +45,7 @@ describe('Day 2 - Toboggan Trajectory', () => {
         const lines = inputStr.split('\r\n')
 
         const start = new Date().getTime()
-        console.log(`solution: ${part1(lines)} calculated in ${new Date().getTime() - start}ms`)
+        console.log(`solution: ${part1(lines, 3, 1)} calculated in ${new Date().getTime() - start}ms`)
     })
 
     it('part 1 - check if the number of letters fits into policy', () => {
@@ -46,13 +53,21 @@ describe('Day 2 - Toboggan Trajectory', () => {
         const numbers = inputStr.split('\r\n')
 
         const start = new Date().getTime()
-        console.log(`solution: ${part1(numbers)} calculated in ${new Date().getTime() - start}ms`)
+        console.log(`solution: ${part1(numbers, 3, 1)} calculated in ${new Date().getTime() - start}ms`)
     })
 
     it('part 2 - sample', () => {
-        const inputStr = "1-3 a: abcde\r\n" +
-            "1-3 b: cdefg\r\n" +
-            "2-9 c: ccccccccc"
+        const inputStr = "..##.......\r\n" +
+            "#...#...#..\r\n" +
+            ".#....#..#.\r\n" +
+            "..#.#...#.#\r\n" +
+            ".#...##..#.\r\n" +
+            "..#.##.....\r\n" +
+            ".#.#.#....#\r\n" +
+            ".#........#\r\n" +
+            "#.##...#...\r\n" +
+            "#...##....#\r\n" +
+            ".#..#...#.#"
         const numbers = inputStr.split('\r\n')
 
         const start = new Date().getTime()
